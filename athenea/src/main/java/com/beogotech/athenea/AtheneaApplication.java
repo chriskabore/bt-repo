8bfe3c42ba2c6f02f2802d87e42e0b97d23d9e6c
Sidnooma Christian Kabore <chriskabor@gmail.com>
2020-04-07 00:26:42 +0000

    edited gitignore edited application.yml file ti add database config

---
+++ athenea/src/main/java/com/beogotech/athenea/AtheneaApplication.java
@@ -0,0 +1,13 @@
+package com.beogotech.athenea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtheneaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AtheneaApplication.class, args);
+	}
+	
+}
---
+++ athenea/src/test/java/com/beogotech/athenea/AtheneaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beogotech.athenea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtheneaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
